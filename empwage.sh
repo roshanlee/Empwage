408b814564c495374071e7176b384af854c92441
roshanlee <roshan26bhagat@gmail.com>
2021-06-09 00:00:48 +0530

    Adder: DailyWages

--- empwage.sh
+++ empwage.sh
@@ -4,7 +4,9 @@ empCheck=$((RANDOM%2))
 isPresent=1
 if(($empCheck==$isPresent))
 then
-	echo Employee present
+	empRatePerHr=20;
+	empHrs=8;
+	salary=$(($empHrs*$empRatePerHr))
 else
-	echo Employee is absent
+	salary=0
 fi
