8d37eb13e94999ca1a1b3a132a1e485710ac15b8
roshanlee <roshan26bhagat@gmail.com>
2021-06-09 10:28:55 +0530

    Employee Wage using CAse Statement

--- empwage.sh
+++ empwage.sh
@@ -4,13 +4,15 @@ empCheck=$((RANDOM%2))
 isFullTime=2
 isPartTime=1
 empRatePerHr=20
-if(($empCheck==$isFullTime))
-then
-	empHrs=8
-elif(($isPartTime==$empCheck))
-then
-	empHrs=4
-else
-	empHrs=0
-fi
+case $empCheck in
+	$isFullTime)
+		empHrs=8
+	;;
+	$isPartTime)
+		empHrs=4
+	;;
+	*)
+		empHrs=0
+	;;
+esac
 salary=$(($empHrs*$empRatePerHr))
