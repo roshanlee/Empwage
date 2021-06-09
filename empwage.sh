fd60fcc84cf503d2ba06608aec365225dfd41533
roshanlee <roshan26bhagat@gmail.com>
2021-06-09 18:02:24 +0530

    Calculateing wage till number of working days

--- empwage.sh
+++ empwage.sh
@@ -8,24 +8,27 @@ NUM_WORKING_DAYS=20
 
 totalEmpHr=0
 totalWorkingDays=0
-
+function getWorkingHours(){
+	 case $empCheck in
+      $isFullTime)
+         empHrs=8
+      ;;
+      $isPartTime)
+         empHrs=4
+      ;;
+      *)
+         empHrs=0
+      ;;
+   esac
+	echo $empHrs
+}
 
 while(($totalEmpHr<$MAX_HRS_IN_MONTH && $totalWorkingDays<$NUM_WORKING_DAYS))
 do
 	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
-	case $empCheck in
-		$isFullTime)
-			empHrs=8
-		;;
-		$isPartTime)
-			empHrs=4
-		;;
-		*)
-			empHrs=0
-		;;
-	esac
-	totalEmpHr=$(($totalEmpHr*$empHrs))
+	workHours="$(getWorkingHours $empCheck)"
+	totalEmpHr=$(($totalEmpHr*$workHours))
 done
 
-totalSalary=$(($totalEmpHr*$EMPRATE_PER_HR))
+totalSalary=$(($totalEmpHr*$EMP_RATE_PER_HR))
